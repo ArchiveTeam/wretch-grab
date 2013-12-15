10becf14683beeb02176ab85fdb86c7d8a7012e5
Christopher Foo <chris.foo@gmail.com>
2013-12-15 22:01:10 +0800

    Add pipeline files.

---
+++ pipeline.py
@@ -0,0 +1,187 @@
+import datetime
+from distutils.version import StrictVersion
+import os
+import seesaw
+from seesaw.config import NumberConfigValue
+from seesaw.externalprocess import WgetDownload
+from seesaw.item import ItemInterpolation, ItemValue
+from seesaw.pipeline import Pipeline
+from seesaw.project import Project
+from seesaw.task import SimpleTask, LimitConcurrent
+from seesaw.tracker import (GetItemFromTracker, SendDoneToTracker,
+    PrepareStatsForTracker, UploadWithTracker)
+from seesaw.util import find_executable
+import shutil
+import time
+
+
+# check the seesaw version
+if StrictVersion(seesaw.__version__) < StrictVersion("0.1.4"):
+    raise Exception("This pipeline needs seesaw version 0.1.4 or higher.")
+
+
+###########################################################################
+# Find a useful Wget+Lua executable.
+#
+# WGET_LUA will be set to the first path that
+# 1. does not crash with --version, and
+# 2. prints the required version string
+WGET_LUA = find_executable(
+    "Wget+Lua",
+    ["GNU Wget 1.14.lua.20130523-9a5c"],
+    [
+        "./wget-lua",
+        "./wget-lua-warrior",
+        "./wget-lua-local",
+        "../wget-lua",
+        "../../wget-lua",
+        "/home/warrior/wget-lua",
+        "/usr/bin/wget-lua"
+    ]
+)
+
+if not WGET_LUA:
+    raise Exception("No usable Wget+Lua found.")
+
+
+###########################################################################
+# The version number of this pipeline definition.
+#
+# Update this each time you make a non-cosmetic change.
+# It will be added to the WARC files and reported to the tracker.
+VERSION = "20131215.00"
+USER_AGENT = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.101 Safari/537.36"
+TRACKER_ID = 'test'
+TRACKER_HOST = 'localhost:8030'
+
+
+###########################################################################
+# This section defines project-specific tasks.
+#
+# Simple tasks (tasks that do not need any concurrency) are based on the
+# SimpleTask class and have a process(item) method that is called for
+# each item.
+class PrepareDirectories(SimpleTask):
+    def __init__(self, warc_prefix):
+        SimpleTask.__init__(self, "PrepareDirectories")
+        self.warc_prefix = warc_prefix
+
+    def process(self, item):
+        item_name = item["item_name"]
+        dirname = "/".join((item["data_dir"], item_name))
+
+        if os.path.isdir(dirname):
+            shutil.rmtree(dirname)
+
+        os.makedirs(dirname)
+
+        item["item_dir"] = dirname
+        item["warc_file_base"] = "%s-%s-%s" % (self.warc_prefix, item_name,
+            time.strftime("%Y%m%d-%H%M%S"))
+
+        open("%(item_dir)s/%(warc_file_base)s.warc.gz" % item, "w").close()
+
+
+class MoveFiles(SimpleTask):
+    def __init__(self):
+        SimpleTask.__init__(self, "MoveFiles")
+
+    def process(self, item):
+        os.rename("%(item_dir)s/%(warc_file_base)s.warc.gz" % item,
+              "%(data_dir)s/%(warc_file_base)s.warc.gz" % item)
+
+        shutil.rmtree("%(item_dir)s" % item)
+
+
+wget_args = [
+    WGET_LUA,
+    "-U", USER_AGENT,
+    "-nv",
+    "-o", ItemInterpolation("%(item_dir)s/wget.log"),
+    "--lua-script", "wretch.lua",
+    "--no-check-certificate",
+    "--output-document", ItemInterpolation("%(item_dir)s/wget.tmp"),
+    "--truncate-output",
+    "-e", "robots=off",
+    "--rotate-dns",
+    "--recursive", "--level=inf",
+    "--page-requisites",
+    "--timeout", "60",
+    "--tries", "inf",
+    "--span-hosts",
+    "--waitretry", "3600",
+    "--domains", "yimg.com,wretch.cc",
+    "--warc-file", ItemInterpolation("%(item_dir)s/%(warc_file_base)s"),
+    "--warc-header", "operator: Archive Team",
+    "--warc-header", "wretch-dld-script-version: " + VERSION,
+    "--warc-header", ItemInterpolation("wretch-user: %(item_name)s"),
+    ItemInterpolation("http://www.wretch.cc/album/%(item_name)s"),
+    ItemInterpolation("http://www.wretch.cc/blog/%(item_name)s"),
+    ItemInterpolation("http://www.wretch.cc/guestbook/%(item_name)s"),
+    ItemInterpolation("http://www.wretch.cc/user/%(item_name)s"),
+    ItemInterpolation("http://www.wretch.cc/friend/%(item_name)s"),
+    ItemInterpolation("http://www.wretch.cc/video/%(item_name)s"),
+]
+
+if 'bind_address' in globals():
+    wget_args.extend(['--bind-address', globals()['bind_address']])
+    print('')
+    print('*** Wget will bind address at {0} ***'.format(globals()['bind_address']))
+    print('')
+
+
+###########################################################################
+# Initialize the project.
+#
+# This will be shown in the warrior management panel. The logo should not
+# be too big. The deadline is optional.
+project = Project(
+    title="Wretch",
+    project_html="""
+    <img class="project-logo" alt="" src="http://archiveteam.org/images/7/76/Archiveteam1.png" height="50" />
+    <h2>Wretch <span class="links"><a href="http://www.wretch.cc/">Website</a> &middot; <a href="http://%s/%s/">Leaderboard</a></span></h2>
+    <p><b>Wretch</b> is killed by Yahoo!.</p>
+    """ % (TRACKER_HOST, TRACKER_ID)
+    , utc_deadline=datetime.datetime(2013, 12, 26, 00, 00, 1)
+)
+
+pipeline = Pipeline(
+    GetItemFromTracker("http://%s/%s" % (TRACKER_HOST, TRACKER_ID), downloader,
+        VERSION),
+    PrepareDirectories(warc_prefix="wretch"),
+    WgetDownload(
+        wget_args,
+        max_tries=5,
+        accept_on_exit_code=[0, 8],
+        env={'wretch_username': ItemInterpolation("%(item_name)s")}
+    ),
+    PrepareStatsForTracker(
+        defaults={ "downloader": downloader, "version": VERSION },
+        file_groups={
+            "data": [ ItemInterpolation("%(item_dir)s/%(warc_file_base)s.warc.gz") ]
+            }
+    ),
+    MoveFiles(),
+    LimitConcurrent(NumberConfigValue(min=1, max=4, default="1",
+        name="shared:rsync_threads", title="Rsync threads",
+        description="The maximum number of concurrent uploads."),
+        UploadWithTracker(
+            "http://tracker.archiveteam.org/%s" % TRACKER_ID,
+            downloader=downloader,
+            version=VERSION,
+            files=[
+                ItemInterpolation("%(data_dir)s/%(warc_file_base)s.warc.gz")
+                ],
+            rsync_target_source_path=ItemInterpolation("%(data_dir)s/"),
+            rsync_extra_args=[
+                "--recursive",
+                "--partial",
+                "--partial-dir", ".rsync-tmp"
+            ]
+            ),
+    ),
+    SendDoneToTracker(
+        tracker_url="http://%s/%s" % (TRACKER_HOST, TRACKER_ID),
+        stats=ItemValue("stats")
+    )
+)
---
+++ wretch.lua
@@ -0,0 +1,69 @@
+local url_count = 0
+local wretch_username = os.getenv("wretch_username")
+
+
+wget.callbacks.download_child_p = function(urlpos, parent, depth, start_url_parsed, iri, verdict, reason)
+  local url = urlpos["url"]["url"]
+
+  -- Don't go outside our username
+  if string.match(url, "wretch%.cc") then
+    if not (
+    string.match(url, "wretch%.cc/[a-z0-9A-Z]-/"..wretch_username.."$") or
+    string.match(url, "wretch%.cc/[a-z0-9A-Z]-/"..wretch_username.."/") or
+    string.match(url, "id="..wretch_username.."$") or
+    string.match(url, "id="..wretch_username.."&")
+    ) then
+      verdict = false
+    end
+  end
+
+  if string.match(url, "wretch%.cc/help/prosecute.php") then
+    verdict = false
+  end
+
+  if verdict then
+    io.stdout:write("Yes: "..url.."\n")
+    io.stdout:flush()
+  end
+
+  return verdict
+end
+
+wget.callbacks.httploop_result = function(url, err, http_stat)
+  local sleep_time = 20
+  local status_code = http_stat["statcode"]
+
+  if status_code >= 500 then
+    io.stdout:write("\nYahoo!!! (code "..http_stat.statcode.."). Sleeping for ".. sleep_time .." seconds.\n")
+    io.stdout:flush()
+
+    -- Note that wget has its own linear backoff to this time as well
+    os.execute("sleep " .. sleep_time)
+    return wget.actions.CONTINUE
+  else
+    -- We're okay; sleep a bit (if we have to) and continue
+    local sleep_time = 1.0 * (math.random(75, 125) / 100.0)
+
+    if string.match(url["url"], "yimg%.com") then
+      -- We should be able to go fast on images since that's what a web browser does
+      sleep_time = 0
+    end
+
+    if sleep_time > 0.001 then
+      os.execute("sleep " .. sleep_time)
+    end
+
+    tries = 0
+    return wget.actions.NOTHING
+  end
+end
+
+wget.callbacks.get_urls = function(file, url, is_css, iri)
+  -- progress message
+  url_count = url_count + 1
+  if url_count % 2 == 0 then
+    io.stdout:write("\r - Downloaded "..url_count.." URLs.")
+    io.stdout:flush()
+  end
+end
+
