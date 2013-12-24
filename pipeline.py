37ec99598bccdca74a1c67e5544fec28049e9314
Christopher Foo <chris.foo@gmail.com>
2013-12-24 15:17:59 +0800

    Decrease sleep to 0.1sec.

--- pipeline.py
+++ pipeline.py
@@ -50,7 +50,7 @@ if not WGET_LUA:
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = "20131224.00"
+VERSION = "20131224.01"
 
 USER_AGENT = random.choice(['Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.57 Safari/537.36',
 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9) AppleWebKit/537.71 (KHTML, like Gecko) Version/7.0 Safari/537.71',
